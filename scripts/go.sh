371972d1f89c7e80249cca4b6761bf70c4811c10
Gery Kahn <geryk@ti.com>
2011-03-13 18:15:11 +0200

    Add cmd to set Android awaik

--- scripts/go.sh
+++ scripts/go.sh
@@ -269,7 +269,10 @@ if [ "$stage_otto" -ne "0" ]; then
 fi
 
 if [ "$stage_sedici" -ne "0" ] && [ "$have_path_to_ini" -ne "0" ]; then
-	./calibrator set ref_nvs $have_path_to_ini
+	echo manual_lock > /sys/power/wake_lock
+	echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+	echo 1 > /sys/devices/system/cpu/cpu1/online
+	echo performance > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
 fi
 
 #
