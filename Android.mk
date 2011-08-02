21e15dae0adbcab29a7f3c0a7438bd01b6444cc6
Ido Reis <idor@ti.com>
2011-08-02 13:32:06 +0300

    Fix Android.mk
    Signed-off-by: Gery Kahn <geryk@ti.com>

--- Android.mk
+++ Android.mk
@@ -24,11 +24,10 @@ LOCAL_MODULE := calibrator
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := scripts/wl12xx-tool.sh
+LOCAL_MODULE := wl12xx-tool.sh
 LOCAL_MODULE_TAGS := eng
-LOCAL_MODULE_PATH := $(local_target_dir)
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES := scripts/$(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
 #
